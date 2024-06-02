88b9d7e48e7606c34f47505a0cd5d78d7d06e519
Pratyaksh Nidhi Sharma <pratyaksh.nidhi@gmail.com>
2024-06-02 19:24:32 -0400

    Re-add apiGateway Server file after removing secrets

---
+++ dist/apiGatewayServer.js
@@ -0,0 +1,66 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+// import { Configuration, OpenAI } from 'openai';
+//import Configuration from 'openai';
+//import OpenAIApi from 'openai';
+//import OpenAI from 'openai';
+const openai_1 = require("openai");
+// import Configuration from 'openai';
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+const api_gateway_server = process.env.API_GATEWAY_SERVER;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(body_parser_1.default.json());
+const apiKey = process.env.OPENAI_API_KEY;
+const openai = new openai_1.OpenAI({ apiKey });
+app.post('/analyzeTone', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('API Gateway Server: Analyzing tone');
+    const emailContent = req.body.emailContent;
+    console.log('API Gateway Server: Email Content found: ', emailContent);
+    //res.send({ tone: 'Tone Analysis Complete' });
+    try {
+        const response = yield openai.chat.completions.create({
+            model: 'gpt-4o',
+            messages: [
+                {
+                    role: "system",
+                    content: "You will be provided with a text which are contents of an email. Please check the tone of the email and give suggestions on how to tone it down. For ex. 'The tone of the email is too aggressive. Here is how you can tone it down....'"
+                },
+                {
+                    role: "user",
+                    content: emailContent
+                }
+            ],
+            max_tokens: 1000
+        });
+        const tone = response.choices[0].message.content;
+        //const tone = response.choices[0].text;
+        console.log('API Gateway Server: Tone Analysis Result: ', tone);
+        return res.json({ tone: tone });
+    }
+    catch (error) {
+        console.error('API Gateway Server: Error in tone analysis: ', error);
+        return res.status(500).send({ message: 'Failed to analyze tone', error: error });
+    }
+}));
+app.listen(port, () => {
+    console.log(`API Gateway Server listening at ${api_gateway_server}:${port}`);
+});
---
+++ src/apiGatewayServer.ts
@@ -0,0 +1,60 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+// import { Configuration, OpenAI } from 'openai';
+//import Configuration from 'openai';
+//import OpenAIApi from 'openai';
+//import OpenAI from 'openai';
+import { OpenAI } from 'openai';
+// import Configuration from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const api_gateway_server = process.env.API_GATEWAY_SERVER;
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+const apiKey = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({ apiKey });
+
+
+app.post('/analyzeTone', async (req: Request, res: Response) => {
+    console.log('API Gateway Server: Analyzing tone');
+    const emailContent = req.body.emailContent;
+    console.log('API Gateway Server: Email Content found: ', emailContent);
+    //res.send({ tone: 'Tone Analysis Complete' });
+    try {
+        const response = await openai.chat.completions.create({
+            model: 'gpt-4o',
+            messages: [
+                { 
+                    role: "system", 
+                    content: "You will be provided with a text which are contents of an email. Please check the tone of the email and give suggestions on how to tone it down. For ex. 'The tone of the email is too aggressive. Here is how you can tone it down....'"
+                },
+                {
+                    role: "user",
+                    content: emailContent
+                }
+            ],
+            max_tokens: 1000
+        });
+        const tone = response.choices[0].message.content;
+        //const tone = response.choices[0].text;
+        console.log('API Gateway Server: Tone Analysis Result: ', tone);
+        return res.json({ tone: tone });
+    } catch (error) {
+        console.error('API Gateway Server: Error in tone analysis: ', error);
+       return res.status(500).send({ message: 'Failed to analyze tone' ,error: error });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`API Gateway Server listening at ${api_gateway_server}:${port}`);
+});
